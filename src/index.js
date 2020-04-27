771f67eaddb858dbd29ddb865f94517981f070b7
Andrew Baird <baird.andrew4@gmail.com>
2020-04-27 14:54:43 -0500

    Added images to pre seed info and working on getting basic infoto show

--- src/index.js
+++ src/index.js
@@ -4,9 +4,25 @@ import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
 
+const authors = [
+	{
+		name: 'Mark Twain',
+		imageUrl: 'images/authors/marktwain.jpg',
+		imageSource: 'Wikipedia Commons',
+		books: ['The Adventures of Huckleberry Finn'],
+	},
+];
+
+const state = {
+	turnData: {
+		author: authors[0],
+		books: authors[0].books,
+	},
+};
+
 ReactDOM.render(
 	<React.StrictMode>
-		<AuthorQuiz />
+		<AuthorQuiz {...state} />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
